6ea7ada003f55767fab3b96bdd92c851341a514a
Daniel Lee <dlee@sheetmusicplus.com>
2017-05-14 17:39:02 -0700

    Add tweeters route

---
+++ app/routes/tweeters.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [{
+      id: 1,
+      uid: "512418287",
+      name: "UCLA Football",
+      screen_name: "UCLAFootball",
+      description: "The official Twitter account of UCLA Football.",
+      created_at: "2017-05-14 21:46:00",
+      updated_at: "2017-05-14 21:46:56"
+    },{
+      id: 2,
+      uid: "524643600",
+      name: "UCLA Basketball",
+      screen_name: "UCLAMBB",
+      description: "The official Twitter account of the UCLA Men's Basketball team.",
+      created_at: "",
+      updated_at: ""
+    }]
+  }
+});
---
+++ tests/acceptance/list-tweeters-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'bruins4sup/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list tweeters');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
