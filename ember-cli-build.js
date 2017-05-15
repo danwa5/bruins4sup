e4354eb1e89199a7242142c9fb7034c9b8dc49f0
Daniel Lee <dlee@sheetmusicplus.com>
2017-05-14 19:48:29 -0700

    Replace ember-bootstrap add-on with ember-cli-sass and bootstrap-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,12 +3,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      'bootstrapVersion': 3,
-      'importBootstrapFont': true,
-      'importBootstrapCSS': true
+    sassOptions: {
+      includePaths: [
+        'bower_components/bootstrap-sass/assets/stylesheets'
+      ]
     }
   });
+
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
