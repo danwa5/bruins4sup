cd98595ed9c332ad95530fd02da0df039249f6fd
Daniel Lee <dlee@sheetmusicplus.com>
2017-06-19 20:49:39 -0700

    Allow user to delete tweeter

--- app/components/tweeter-listing.js
+++ app/components/tweeter-listing.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  actions: {
+    deleteTweeter(tweeter) {
+      this.get('store').findRecord('tweeter', tweeter.id, { backgroundReload: false }).then(function(tweeter) {
+        tweeter.destroyRecord(); // => DELETE to /posts/2
+      });
+    }
+  }
 });
